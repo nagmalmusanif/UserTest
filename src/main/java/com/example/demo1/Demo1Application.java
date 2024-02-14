ddcec4c039acceb1e2221517aecf0e7506afe7d0
pc <al@>
2024-02-14 11:27:04 +0300

    ggfff

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -10,6 +10,9 @@ public class Demo1Application {
 
 		SpringApplication.run(Demo1Application.class, args);//
 		//this is develop bracg
+		//this is develop bracg
+		//this is develop bracg
+
 	}
 
 }
